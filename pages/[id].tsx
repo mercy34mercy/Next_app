54daa1a3690d71b86d6b02eb776658a73e1201c9
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-09-11 17:39:57 +0900

    フォルダ構造変更

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next';
-import type { Blog } from '../blog';
-import client from '../client';
+import type { Blog } from './blog';
+import client from './client';
 import '../../styles/[id].module.css'
 
 type Props = {
